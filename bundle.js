38833380e966a24f0dd221f3c52554048c4a1dfb
Artem <artem@MacBook-Air-Artem.local>
2022-10-21 18:12:04 +0300

    release_v1

---
+++ bundle.js
@@ -0,0 +1,226 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/css/animations.css":
+/*!********************************!*\
+  !*** ./src/css/animations.css ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/animations.css?");
+
+/***/ }),
+
+/***/ "./src/css/footer.css":
+/*!****************************!*\
+  !*** ./src/css/footer.css ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/footer.css?");
+
+/***/ }),
+
+/***/ "./src/css/header.css":
+/*!****************************!*\
+  !*** ./src/css/header.css ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/header.css?");
+
+/***/ }),
+
+/***/ "./src/css/main.css":
+/*!**************************!*\
+  !*** ./src/css/main.css ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/main.css?");
+
+/***/ }),
+
+/***/ "./src/css/null.css":
+/*!**************************!*\
+  !*** ./src/css/null.css ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/null.css?");
+
+/***/ }),
+
+/***/ "./src/css/section_about.css":
+/*!***********************************!*\
+  !*** ./src/css/section_about.css ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/section_about.css?");
+
+/***/ }),
+
+/***/ "./src/css/section_contacts.css":
+/*!**************************************!*\
+  !*** ./src/css/section_contacts.css ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/section_contacts.css?");
+
+/***/ }),
+
+/***/ "./src/css/section_main.css":
+/*!**********************************!*\
+  !*** ./src/css/section_main.css ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/section_main.css?");
+
+/***/ }),
+
+/***/ "./src/css/section_myskills.css":
+/*!**************************************!*\
+  !*** ./src/css/section_myskills.css ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/section_myskills.css?");
+
+/***/ }),
+
+/***/ "./src/css/section_portfolio.css":
+/*!***************************************!*\
+  !*** ./src/css/section_portfolio.css ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://portfolio/./src/css/section_portfolio.css?");
+
+/***/ }),
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_portfolio__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/portfolio */ \"./src/js/modules/portfolio.js\");\n/* harmony import */ var _modules_footer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/footer */ \"./src/js/modules/footer.js\");\n/* harmony import */ var _modules_menuTabs__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/menuTabs */ \"./src/js/modules/menuTabs.js\");\n/* harmony import */ var _modules_script__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/script */ \"./src/js/modules/script.js\");\n/* harmony import */ var _css_null_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../css/null.css */ \"./src/css/null.css\");\n/* harmony import */ var _css_main_css__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../css/main.css */ \"./src/css/main.css\");\n/* harmony import */ var _css_header_css__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../css/header.css */ \"./src/css/header.css\");\n/* harmony import */ var _css_section_main_css__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../css/section_main.css */ \"./src/css/section_main.css\");\n/* harmony import */ var _css_section_portfolio_css__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../css/section_portfolio.css */ \"./src/css/section_portfolio.css\");\n/* harmony import */ var _css_section_about_css__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ../css/section_about.css */ \"./src/css/section_about.css\");\n/* harmony import */ var _css_section_myskills_css__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ../css/section_myskills.css */ \"./src/css/section_myskills.css\");\n/* harmony import */ var _css_section_contacts_css__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ../css/section_contacts.css */ \"./src/css/section_contacts.css\");\n/* harmony import */ var _css_footer_css__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ../css/footer.css */ \"./src/css/footer.css\");\n/* harmony import */ var _css_animations_css__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ../css/animations.css */ \"./src/css/animations.css\");\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\nwindow.addEventListener('DOMContentLoaded', () => {\n    'use strict'\n\n    ;(0,_modules_portfolio__WEBPACK_IMPORTED_MODULE_0__.portfolio)()\n    ;(0,_modules_menuTabs__WEBPACK_IMPORTED_MODULE_2__.menuTabs)()\n    ;(0,_modules_script__WEBPACK_IMPORTED_MODULE_3__.script)()\n    ;(0,_modules_footer__WEBPACK_IMPORTED_MODULE_1__.footer)()\n})\n\n//# sourceURL=webpack://portfolio/./src/js/index.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/footer.js":
+/*!**********************************!*\
+  !*** ./src/js/modules/footer.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"footer\": () => (/* binding */ footer)\n/* harmony export */ });\nfunction footer() {\n\n    const footerElements = document.querySelectorAll('.footer__wrapper-anim');\n\n    footerElements.forEach(footerItem => {\n        fixedFooterRevealAnim(footerItem);\n    })\n\n\n    function fixedFooterRevealAnim(element) {\n        const preLastSection = document.querySelector('.contact-form'),\n            footerElement = element.getBoundingClientRect().bottom,\n            preLastSectionHover = preLastSection.getBoundingClientRect().bottom;\n\n        window.addEventListener('scroll', (e) => {\n            if (window.scrollY >= preLastSectionHover - footerElement) {\n                element.classList.add('_active')\n            }\n        })\n    }\n}\n\n \n\n//# sourceURL=webpack://portfolio/./src/js/modules/footer.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/menuTabs.js":
+/*!************************************!*\
+  !*** ./src/js/modules/menuTabs.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"menuTabs\": () => (/* binding */ menuTabs)\n/* harmony export */ });\nfunction menuTabs() {\n\n    const menuBlocks = document.querySelectorAll('.menu'),\n          mouseLink = document.querySelector('.info__scroll-link');\n\n    menuBlocks.forEach(menuBlock => {\n        menuBlock.addEventListener('click', (e) => {\n            e.preventDefault();\n            if (e.target.classList.contains('menu__link')) {\n\n                scrollTo(e.target);\n            }\n        })\n    })\n\n    mouseLink.addEventListener('click', (e) => {\n        e.preventDefault();\n        scrollTo(mouseLink);\n    })\n\n\n\n    function scrollTo(element) {\n        const section = element.getAttribute('data-section');\n        if (section == '.footer') {\n            window.scrollTo({\n                top: document.documentElement.scrollHeight,\n                behavior: 'smooth'\n            });\n        } else {\n\n\n            const position = document.querySelector(`${section}`).getBoundingClientRect().top;\n            window.scrollTo({\n                top: position,\n                behavior: 'smooth'\n\n            });\n        }\n    }\n}\n\n \n\n//# sourceURL=webpack://portfolio/./src/js/modules/menuTabs.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/portfolio.js":
+/*!*************************************!*\
+  !*** ./src/js/modules/portfolio.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"portfolio\": () => (/* binding */ portfolio)\n/* harmony export */ });\nfunction portfolio() {\n\n  const portfolioItems = document.querySelectorAll('.img__animation-wrapper');\n\n  portfolioItems.forEach(item => {\n    elementScrollAnimation(item, '_anim');\n  })\n\n  function elementScrollAnimation (element, cssClassModification) {\n      document.addEventListener('scroll', (event) => {\n        event.preventDefault();\n        const distance = element.getBoundingClientRect();\n        if (distance.top + (window.innerHeight / 8) < window.innerHeight && distance.bottom - (window.innerHeight / 8) > 0){\n          element.classList.add(cssClassModification);\n        }\n      })\n    }\n  \n}\n\n\n\n//# sourceURL=webpack://portfolio/./src/js/modules/portfolio.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/script.js":
+/*!**********************************!*\
+  !*** ./src/js/modules/script.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"script\": () => (/* binding */ script)\n/* harmony export */ });\nfunction script() {\n  const burgerBtn = document.querySelector('.header__menu-icon'),\n    burgerMenu = document.querySelector('.header__nav'),\n    burgerBtnElement = document.querySelectorAll('.menu-icon__element'),\n    mainBody = document.querySelector('body');\n\n  burgerBtn.addEventListener('click', (e) => {\n    burgerMenu.classList.toggle('_active');\n    burgerBtnElement.forEach(item => {\n      item.classList.toggle('_active')\n    });\n    mainBody.classList.toggle('_lock')\n  });\n  // --------------------------------------------------\n  const elements = document.querySelectorAll('.object__wrapper-anim');\n\n  elements.forEach(item => elementScrollAnimation(item, '_active'));\n\n  // ---------------------------------------------------------------------My_skills_counter_anim\n\n  let counters = document.querySelectorAll('.counter__level'),\n    countersParrent = document.querySelectorAll('.counter__item');\n\n  for (let i = 0; i < countersParrent.length; i++) {\n    elementScrollAnimation(countersParrent[i], `_reveal_counter`);\n  }\n\nconsole.log(countersParrent)\n  countersParrent.forEach(parrent => {\n    const letir = setInterval(() => {\n      if (parrent.classList.contains('_reveal_counter')) {\n        counters.forEach(counter => {\n            outNum(counter);\n          clearInterval(letir);\n        })\n      }\n    }, 10);\n  })\n\n  function outNum (element) {\n  const time = 1500; //ms\n  let start = 0,\n      deadline = +element.id,\n      incr = 1,\n      interval = Math.ceil(time / deadline);\n      const cycle = setInterval( () => {\n        start = start + incr;\n        element.innerHTML = start\n        if (start >= deadline) {\n          element.innerHTML = deadline;\n          clearInterval(cycle);\n        }\n      }, interval)\n  \n\n  }\n\n  function elementScrollAnimation (element, cssClassModification) {\n    document.addEventListener('scroll', (event) => {\n      event.preventDefault();\n      const distance = element.getBoundingClientRect();\n      if (distance.top + (window.innerHeight / 8) < window.innerHeight && distance.bottom - 10 > 0){\n        element.classList.add(cssClassModification);\n      }\n    })\n  }\n}\n\n\n\n//# sourceURL=webpack://portfolio/./src/js/modules/script.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/index.js");
+/******/ 	
+/******/ })()
+;
